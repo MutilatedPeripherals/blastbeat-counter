6ffd208051fd4db8a168e22b18825c9a02832cf5
linomp <linomp95@hotmail.com>
2025-09-16 23:27:27 +0300

    update tests & add gh actions

--- test_main.py
+++ test_main.py
@@ -3,13 +3,13 @@ from main import LabeledSection, identify_blastbeat_intervals
 
 def test_identify_blasts_1():
     input: list[LabeledSection] = [
-        LabeledSection((1,2), False, True),
-        LabeledSection((2,3), True, True),
-        LabeledSection((3,4), True, True),
-        LabeledSection((4,5), True, True),
-        LabeledSection((5,6), True, True),
-        LabeledSection((6,7), True, False),
-        LabeledSection((7,8), True, False),
+        LabeledSection(1,2, False, True),
+        LabeledSection(2,3, True, True),
+        LabeledSection(3,4, True, True),
+        LabeledSection(4,5, True, True),
+        LabeledSection(5,6, True, True),
+        LabeledSection(6,7, True, False),
+        LabeledSection(7,8, True, False),
     ]
 
     expected = [(2,6)]
@@ -21,14 +21,14 @@ def test_identify_blasts_1():
 
 def test_identify_blasts_2():
     input: list[LabeledSection] = [
-        LabeledSection((1,2), False, True),
-        LabeledSection((2,3), True, True),
-        LabeledSection((3,4), True, True),
-        LabeledSection((4,5), True, False),
-        LabeledSection((5,6), True, True),
-        LabeledSection((6,7), True, True),
-        LabeledSection((7,8), True, True),
-        LabeledSection((8,9), True, False),
+        LabeledSection(1,2, False, True),
+        LabeledSection(2,3, True, True),
+        LabeledSection(3,4, True, True),
+        LabeledSection(4,5, True, False),
+        LabeledSection(5,6, True, True),
+        LabeledSection(6,7, True, True),
+        LabeledSection(7,8, True, True),
+        LabeledSection(8,9, True, False),
     ]
 
     expected = []
